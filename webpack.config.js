5b093b2296632a69fda15fd79b87ab2338fc2775
Kaat Vermeire <kaat.vermeire@student.howest.be>
2021-01-07 18:36:31 +0100

    startproject webpack

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    output: {
+        path: require('path').resolve(`./dist/bundled/js`),
+        filename: `index.js`,
+    },
+    module: {
+        rules: [
+        ]
+    }
+}; 
