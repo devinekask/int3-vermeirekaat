3f04485c85bdbc8ab6dec3435644ae1f77723a93
Kaat Vermeire <kaat.vermeire@student.howest.be>
2021-01-10 16:51:46 +0100

    homepage - html #9

---
+++ src/view/site/index.php
@@ -0,0 +1,38 @@
+<section class="header">
+  <h1 class="header__title">Maker Faire Gent presents</h1>
+  <p class="header__subtitle">High Wheeling</p>
+  <img src="./assets/collages/collage-header.png" alt="header-collage">
+</section>
+
+<section class="content">
+<h1 class="hidden">High Wheeling</h1>
+  <article class="content__item content__part-one">
+    <div class="content__bg-image__top scroll--overlay"></div>
+    <h2 class="content__title">High What?</h3>
+      <p class="content__alinea">High Wheeling was de aanleiding tot fietsen als sport. <br>Het vervoersmiddel werd uitgevonden het het <span>Victoriaanse tijdperk</span> en was erg populair voor een korte periode.</p>
+      <p class="content__alinea">De <span>Maker Faire</span> daagt jou uit om dit toestel terug tot leven te brengen. <br> Het festival voor innovators en creatievelingen wil je zo motiveren om actief bezig te zijn.</p>
+
+      <img src="./assets/collages/collage-skyline.png" alt="collage-skyline">
+  </article>
+
+  <article class="content__item content__part--two">
+    <h2 class="content__title">Get Started</h3>
+    <p class="content__alinea">Steek zelf je handen uit de mouwen en bouw je <span>eigen High Wheel</span>. <br> Ontpop jezelf tot de nieuwe 'Bob(ette) de Bouwer' en doorkruis het hele land als een Victoriaanse koning(in).</p>
+
+    <a class="cta cta--yellow" href="index.php?page=">Ready - Set - Go</a>
+
+    <p class="content__alinea alinea__promo">Bezoek het festival op <span>30 april</span> / <span>1 mei</span> / <span>2 mei</span> en aanschouw tal van creatieve innovaties uit eigen land.</p>
+  </article>
+
+  <article>
+    <img src="./assets/collages/collage-diy.png" alt="collage-diy">
+    <h2 class="content__title">Build with Creativity</h3>
+    <p class="content__alinea">Om de High Wheel zelf te bouwen, heb je bepaalde materialen nodig. <br> In de webshop kan je <span>basispakketten</span> bestellen zodat je al de nodige spullen in huis hebt om eraan te beginnen.</p>
+
+    <a class="cta cta--cyan" href="index.php?page=">Bestel jouw bouwpakket</a>
+    <div class="content__bg-image__bottom scroll--overlay"></div>
+  </article>
+
+</section>
+
+
