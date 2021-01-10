69881994a026d3dfb2c1075e4b091c62869f1fca
Kaat Vermeire <kaat.vermeire@student.howest.be>
2021-01-10 18:48:49 +0100

    adding perspective hover to img #20

--- src/view/site/index.php
+++ src/view/site/index.php
@@ -3,7 +3,7 @@
 <section class="header">
   <h1 class="header__title">Maker Faire Gent presents</h1>
   <p class="header__subtitle">High Wheeling</p>
-  <img width="960" src="./assets/collages/collage-header.png" alt="header-collage">
+  <img class="img--perspective" width="960" src="./assets/collages/collage-header.png" alt="header-collage">
 </section>
 
 <section class="content">
@@ -14,7 +14,10 @@
       <p class="content__alinea">High Wheeling was de aanleiding tot fietsen als sport. <br>Het vervoersmiddel werd uitgevonden het het <span>Victoriaanse tijdperk</span> en was erg populair voor een korte periode.</p>
       <p class="content__alinea">De <span>Maker Faire</span> daagt jou uit om dit toestel terug tot leven te brengen. <br> Het festival voor innovators en creatievelingen wil je zo motiveren om actief bezig te zijn.</p>
 
-      <img src="./assets/collages/collage-skyline.png" alt="collage-skyline">
+      <div class="img__container">
+        <img class="img--perspective" width="960" src="./assets/collages/collage-skyline.png" alt="collage-skyline">
+      </div>
+
   </article>
 
   <article class="content__item content__part--two">
@@ -25,13 +28,11 @@
     <a class="cta cta--yellow" href="index.php?page=">Ready - Set - Go</a>
     </div>
 
-
-
     <p class="content__alinea alinea__promo">Bezoek het festival op <span>30 april</span> / <span>1 mei</span> / <span>2 mei</span> en aanschouw tal van creatieve innovaties uit eigen land.</p>
   </article>
 
   <article class="content__item content__part-three">
-    <img src="./assets/collages/collage-diy.png" alt="collage-diy">
+    <img class="img--perspective" width="960" src="./assets/collages/collage-diy.png" alt="collage-diy">
     <h2 class="content__title">Build with Creativity</h3>
     <p class="content__alinea">Om de High Wheel zelf te bouwen, heb je bepaalde materialen nodig. <br> In de webshop kan je <span>basispakketten</span> bestellen zodat je al de nodige spullen in huis hebt om eraan te beginnen.</p>
 
