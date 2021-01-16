2e170d6445e014e215b0a5a8af1716de6c1ea681
Kaat Vermeire <kaat.vermeire@student.howest.be>
2021-01-16 15:51:50 +0100

    finishing styling elements in mobile #9

--- src/view/site/index.php
+++ src/view/site/index.php
@@ -1,40 +1,53 @@
-<header class="nav">
-  <img class="nav__logo" src="./assets/img/logo_maker-faire.png" alt="maker-faire-gent">
-  <ul class="nav__buttons">
-    <li class="nav__item"><a class="nav__item--link link cta" href="index.php?page=">Bouwpakket</a></li>
-    <li class="nav__item"><a class="nav__item--link link cta" href="index.php?page=">Aan de slag</a></li>
-  </ul>
-</header>
-
 <div class="home">
+
+<header class="nav">
+  <div class="nav__wrapper">
+    <img class="nav__logo" src="./assets/img/logo_maker-faire.png" alt="maker-faire-gent">
+    <div class="buttons__wrapper">
+      <ul class="nav__buttons">
+        <li class="nav__item"><a class="nav__item--link link cta--secondary" href="index.php?page=">Bouwpakket</a></li>
+        <li class="nav__item"><a class="nav__item--link link cta--primary" href="index.php?page=">Aan de slag</a></li>
+      </ul>
+    </div>
+
+  </div>
+
+</header>
 
 <section class="header">
   <h1 class="header__title">Maker Faire Gent presents</h1>
+
+  <div class="header__wrapper">
     <p class="header__subtitle">High Wheeling</p>
 
-  <picture class="header__picture">
-    <source sizes="(min-width: 450px) 30vw,
-      (min-width: 700px) 44vw, 1vw"
+    <picture class="header__picture">
+      <source sizes="(min-width: 450px) 30vw,
+        (min-width: 700px) 44vw, 1vw"
       srcset="./assets/collages/collage_header.webp 572w,
-          ./assets/collages/collage_header@0.5x.webp 350w"
+        ./assets/collages/collage_header@0.5x.webp 350w"
       type="image/webp" />
 
       <source sizes="(min-width: 450px) 30vw,
-      (min-width: 700px) 44vw, 1vw"
+        (min-width: 700px) 44vw, 1vw"
       srcset="./assets/collages/collage_header.png 527w,
-          ./assets/collages/collage_header@0.5x.png 350w"
+        ./assets/collages/collage_header@0.5x.png 350w"
       type="image/png" />
 
       <img class="header__img img--perspective" height="846" width="839" alt="collage-header" src="./assets/collages/collage_header.png">
-    </picture>
+  </picture>
+  </div>
+
 </section>
 
 <section class="content">
 <h1 class="hidden">High Wheeling</h1>
   <article class="content__item content__part-one">
+    <div class="content__text">
     <h2 class="content__title">High What?</h3>
       <p class="content__alinea">High Wheeling was de aanleiding tot fietsen als sport. <br>Het vervoersmiddel werd uitgevonden het <span>Victoriaanse tijdperk</span> en was erg populair voor een korte periode.</p>
       <p class="content__alinea">De <span>Maker Faire</span> daagt jou uit om dit voertuig terug tot leven te brengen. <br> Het festival voor innovators en creatievelingen wil je zo motiveren om actief bezig te zijn.</p>
+    </div>
+
 
     <picture class="what__picture">
       <source sizes="(min-width: 450px) 30vw,
@@ -54,33 +67,44 @@
   </article>
 
   <div class="promo">
+    <div class="promo__wrapper">
     <p class="promo__content">Bezoek de Maker Faire in de <span>Chinastraat</span> te <span>Gent</span> en ontdek er tal van innovatieve projecten uit eigen bodem!</p>
+    </div>
+
   </div>
 
   <article class="content--horizontal">
     <h2 class="content__title--extra">Get Started</h3>
-    <p class="content__title">What do you need?</p>
+    <p class="header__subtitle subtitle--extra">What do you need?</p>
 
-    <div class="horizontal__part part--one">
-      <h3 class="horizontal__title">Some Tools</h3>
-      <p class="horizontal__alinea">Uiteraard heb je bepaalde materialen nodig om aan dit project te werken. <br>Je kan altijd een bouwpakket samenstellen en bestellen om thuis mee aan de slag te gaan.</p>
-      <a class="cta" href="index.php?page=">Bestel een bouwpakket</a>
-    </div>
+    <div class="horizontal__wrapper">
 
-    <div class="horizontal__part part--two">
-      <h3 class="horizontal__title">Lots of Excitement</h3>
-      <p class="horizontal__alinea">We raden je ssterk aan om met vol enthousiasme aan dit project te starten. <br>Haal de speelvogel in jezelf naar boven en beleef een moment vol nieuwsgierigheid en plezier!</p>
-    </div>
+      <div class="horizontal__part part--one">
+        <h3 class="horizontal__title">Some Tools</h3>
+        <p class="horizontal__alinea">Uiteraard heb je bepaalde materialen nodig om aan dit project te werken. <br>Je kan altijd een bouwpakket samenstellen en bestellen om thuis mee aan de slag te gaan.</p>
+        <div class="cta__container">
+          <a class="cta" href="index.php?page=">Bestel een bouwpakket</a>
+        </div>
+      </div>
 
-    <div class="horizontal__part part--three">
-      <h3 class="horizontal__title">Two Left Hands</h3>
-      <p class="horizontal__alinea">Zelf iets bouwen is niet voor iedereen weggelegd maar jij kan het zeker! <br>Stap voor stap leggen we je uit hoe je de High Wheel tot z'n recht maakt, easy-peasy-lemon-squeezy</p>
-      <a class="cta" href="index.php?page=">Bestel een bouwpakket</a>
+      <div class="horizontal__part part--two">
+        <h3 class="horizontal__title">Lots of Excitement</h3>
+        <p class="horizontal__alinea">We raden je ssterk aan om met vol enthousiasme aan dit project te starten. <br>Haal de speelvogel in jezelf naar boven en beleef een moment vol nieuwsgierigheid en plezier!</p>
+      </div>
+
+      <div class="horizontal__part part--three">
+        <h3 class="horizontal__title">Two Left Hands</h3>
+        <p class="horizontal__alinea">Zelf iets bouwen is niet voor iedereen weggelegd maar jij kan het zeker! <br>Stap voor stap leggen we je uit hoe je de High Wheel tot z'n recht maakt, easy-peasy-lemon-squeezy</p>
+        <div class="cta__container">
+          <a class="cta" href="index.php?page=">Ga aan de slag</a>
+      </div>
     </div>
   </article>
 
   <div class="promo">
-    <p class="promo__content">Bezoek het festival op <span>30 april / 1 mei / 2 mei</span> en aanschouw tal van creatieve innovaties uit eigen land!</p>
+    <div class="promo__wrapper">
+      <p class="promo__content">Bezoek het festival op <span>30 april / 1 mei / 2 mei</span> en aanschouw tal van creatieve innovaties uit eigen land!</p>
+    </div>
   </div>
 
   <article class="content__item content__part-three">
@@ -118,35 +142,40 @@
     <p class="line__info">30 April - 1 Mei - 2 Mei  || 2021</p>
   </div>
 
-  <div class="info">
-    <div class="info__maker">
-      <p class="maker__title">Maker Faire Gent</p>
-      <p class="maker__content">Het festival voor creatievelingen, uitvinders en speelvogels</p>
+  <div class="footer__wrapper">
+    <div class="info">
+      <div class="info__maker">
+        <p class="maker__title">Maker Faire Gent</p>
+        <p class="maker__content">Het festival voor creatievelingen, uitvinders en speelvogels</p>
+      </div>
+      <div class="info__abbo">
+        <p class="footer__subtitle">Blijf op de hoogte</p>
+          <div class="abbo__input">
+            <p class="abbo__email">maker.faire@mail.com</p>
+            <p class="abbo__cta">Schrijf me in</p>
+          </div>
+      </div>
     </div>
-    <div class="info__abbo">
-      <p class="footer__subtitle">Blijf op de hoogte</p>
-        <div class="abbo__input">
-          <p class="abbo__email">maker.faire@mail.com</p>
-          <p class="abbo__cta">Schrijf me in</p>
-        </div>
+
+    <div class="social">
+      <p class="footer__subtitle">Volg ons op</p>
+      <div class="social__wrapper">
+        <img class="social__icon" width="40" height="40" src="./assets/social/social-facebook.png" alt="logo-facebook">
+        <img class="social__icon" width="40" src="./assets/social/social-instagram.png" alt="logo-instagram">
+        <img class="social__icon" width="40" src="./assets/social/social-twitter.png" alt="logo-twitter">
+      </div>
     </div>
-  </div>
 
-  <div class="social">
-    <p class="footer__subtitle">Volg ons op</p>
-      <img class="social__icon" src="./assets/social/social-facebook.png" alt="logo-facebook">
-      <img class="social__icon" src="./assets/social/social-instagram.png" alt="logo-instagram">
-      <img class="social__icon" src="./assets/social/social-twitter.png" alt="logo-twitter">
-  </div>
+    <div class="sponsors">
+      <img class="sponsors__img" width="150" src="./assets/sponsors/sponsor_make-magazine.png" alt="logo-maker-magazine">
+      <img class="sponsors__img" width="150" src="./assets/sponsors/sponsor_flanders.png" alt="logo-flanders">
+      <img class="sponsors__img" width="150" src="./assets/sponsors/sponsor_gent.png" alt="logo-gent">
+      <img class="sponsors__img" width="150" src="./assets/sponsors/sponsor_ugent.png" alt="logo-ugent">
+      <img class="sponsors__img" width="150" src="./assets/sponsors/sponsor_velleman.png" alt="logo-velleman">
+      <img class="sponsors__img" width="150" src="./assets/sponsors/sponsor_accent.png" alt="logo-accent">
+      <img class="sponsors__img sponsors--last" width="150" src="./assets/sponsors/sponsor_tentzetter.png" alt="logo-tentzetter">
+    </div>
 
-  <div class="sponsors">
-    <img class="sponsors__img" width="100" src="./assets/sponsors/sponsor_make-magazine.png" alt="logo-maker-magazine">
-    <img class="sponsors__img" width="100" src="./assets/sponsors/sponsor_flanders.png" alt="logo-flanders">
-    <img class="sponsors__img" width="100" src="./assets/sponsors/sponsor_gent.png" alt="logo-gent">
-    <img class="sponsors__img" width="100" src="./assets/sponsors/sponsor_ugent.png" alt="logo-ugent">
-    <img class="sponsors__img" width="100" src="./assets/sponsors/sponsor_velleman.png" alt="logo-velleman">
-    <img class="sponsors__img" width="100" src="./assets/sponsors/sponsor_accent.png" alt="logo-accent">
-    <img class="sponsors__img" width="100" src="./assets/sponsors/sponsor_tentzetter.png" alt="logo-tentzetter">
   </div>
 </footer>
 
