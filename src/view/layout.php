905b6b4973ea9821e35288ab9f5a8c0c87a913ca
Kaat Vermeire <kaat.vermeire@student.howest.be>
2021-01-17 13:13:04 +0100

    adding horizontal scroll with assets #25

--- src/view/layout.php
+++ src/view/layout.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="UTF-8">
-  <!-- <meta name="viewport" content="initial-scale=1.0, width=device-width">-->
+  <meta name="viewport" content="initial-scale=1.0, width=device-width">
   <title>High Wheeler - <?php echo $title; ?></title>
   <?php echo $css; ?>
   </head>
