30d8ddfb6076df754042f0ec8f53960523440fe0
Kaat Vermeire <kaat.vermeire@student.howest.be>
2021-01-20 15:45:38 +0100

    ready for heroku #29

--- src/dao/DAO.php
+++ src/dao/DAO.php
@@ -1,27 +1,27 @@
-<?php 
+<?php
 
 class DAO {
-    
-    /* // Properties
-    private static $sharedPDO; 
-    protected $pdo; 
 
-    // Constructor 
+  // Properties
+    private static $sharedPDO;
+    protected $pdo;
+
+    // Constructor
     function __construct() {
 
         if(empty(self::$sharedPDO)) {
-            $dbHost = getenv('PHP_DB_HOST') ?: "localhost"; 
-            $dbName = getenv('PHP_DB_DATABASE') ?: "site"; 
-            $dbUser = getenv('PHP_DB_USERNAME') ?: "site"; 
-            $dbPass = getenv('PHP_DB_PASSWORD') ?: "site"; 
+            $dbHost = getenv('PHP_DB_HOST') ?: "localhost";
+            $dbName = getenv('PHP_DB_DATABASE') ?: "highwheel";
+            $dbUser = getenv('PHP_DB_USERNAME') ?: "highwheel";
+            $dbPass = getenv('PHP_DB_PASSWORD') ?: "highwheeldb";
 
             self::$sharedPDO = new PDO("mysql:host=" . $dbHost . ";dbName=" . $dbName, $dbUser, $dbPass);
-            self::$sharedPDO->exec("SET CHARACTER SET utf8"); 
-            self::$sharedPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMPDE_EXCEPTION); 
-            self::$sharedPDO->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); 
+            self::$sharedPDO->exec("SET CHARACTER SET utf8");
+            self::$sharedPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMPDE_EXCEPTION);
+            self::$sharedPDO->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         }
-        $this->pdo = &self::$sharedPDO; 
+        $this->pdo = &self::$sharedPDO;
     }
 
-    // Methods */ 
+    // Methods
 }
