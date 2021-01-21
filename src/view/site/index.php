9f5825e7863dbdfd9f2d66ab90b04fad5e78285f
Kaat Vermeire <kaat.vermeire@student.howest.be>
2021-01-21 19:00:29 +0100

    horizontal part responsive #31 #34

--- src/view/site/index.php
+++ src/view/site/index.php
@@ -90,34 +90,34 @@
     </article>
 
 
-  <!-- <article class="content--horizontal background__img">
+  <article class="content__horizontal background__img">
     <div class="horizontal__header">
       <h2 class="title title--extra">Get Started</h3>
       <p class="header__subtitle subtitle">What do you need?</p>
     </div>
 
-    <div class="horizontal--grid">
+    <div class="horizontal__route">
           <div class="route__part route__part-one">
             <img class="route__ellipse ellipse-one" type="svg" src="./assets/svg/ellipse.svg" alt="yellow-ellipse">
             <hr class="route__dashed-line route-one">
           </div>
-          <div class="route__part route__part-two">
+          <!-- <div class="route__part route__part-two">
             <img class="route__ellipse ellipse-two" type="svg" src="./assets/svg/ellipse.svg" alt="yellow-ellipse">
               <hr class="route__dashed-line route-two">
           </div>
           <div class="route__part route__part-three">
             <img class="route__ellipse ellipse-three" type="svg" src="./assets/svg/ellipse.svg" alt="yellow-ellipse">
             <hr class="route__dashed-line route-three">
-          </div>
+          </div>-->
+      </div>
 
-      <div class="horizontal__overflow">
         <div class="horizontal__wrapper">
         <div class="horizontal__part part-one">
           <h3 class="horizontal__title">Some Tools</h3>
-            <p class="horizontal__alinea">Uiteraard heb je bepaalde materialen nodig om aan dit project te werken. <br>Je kan altijd een bouwpakket samenstellen en bestellen om thuis mee aan de slag te gaan.</p>
+            <p class="horizontal__alinea">Uiteraard heb je bepaalde materialen nodig om aan dit project te werken. <br>Je kan altijd een Building Box bestellen om thuis mee aan de slag te gaan.</p>
             <div class="cta__container">
               <a class="cta cta--regular" href="index.php?page=">Bestel een bouwpakket</a>
-            </div>
+        </div>
         </div>
 
         <div class="horizontal__part part-two opacity">
@@ -126,17 +126,15 @@
         </div>
 
         <div class="horizontal__part part-three opacity">
-          <h3 class="horizontal__title">Two Left Hands</h3>
+          <h3 class="horizontal__title horizontal--left">Two <span>Left</span> Hands</h3>
+          <p class="horizontal__title horizontal--right"> <span>Two</span>  Right</p>
             <p class="horizontal__alinea">Zelf iets bouwen is niet voor iedereen weggelegd maar jij kan het zeker! <br>Stap voor stap leggen we je uit hoe je de High Wheel tot z'n recht maakt, easy-peasy-lemon-squeezy</p>
             <div class="cta__container">
               <a class="cta cta--regular" href="index.php?page=">Ga aan de slag</a>
             </div>
         </div>
-      </div>
-    </div>
-    </div>
 
-  </article> -->
+  </article>
 
   <article class="promo--circ promo--second">
       <div class="promo__wrapper">
