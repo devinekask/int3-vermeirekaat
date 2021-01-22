b9ff116c49e3650be27ab327e7965f1ea52dca10
Kaat Vermeire <kaat.vermeire@student.howest.be>
2021-01-22 15:38:12 +0100

    tutorial responsive on larger screens #10 #39

--- src/view/site/index.php
+++ src/view/site/index.php
@@ -60,7 +60,7 @@
           <p class="content__alinea">High Wheeling was de aanleiding tot fietsen als sport. <br>Het vervoersmiddel werd uitgevonden het <span>Victoriaanse tijdperk</span> en was erg populair voor een korte periode.</p>
           <p class="content__alinea">De <span>Maker Faire</span> daagt jou uit om dit voertuig terug tot leven te brengen. <br> Het festival voor innovators en creatievelingen wil je zo motiveren om actief bezig te zijn.</p>
           <div class="cta__container">
-            <a class="cta cta--regular" href="index.php?page=">Start met bouwen</a>
+            <a class="cta cta--regular" href="index.php?page=tutorial">Start met bouwen</a>
           </div>
       </div>
 
@@ -141,7 +141,7 @@
           <div class="horizontal--pos">
           <p class="horizontal__alinea">Zelf iets bouwen is niet voor iedereen weggelegd maar jij kan het zeker! <br>Stap voor stap leggen we je uit hoe je de High Wheel tot z'n recht maakt, easy-peasy-lemon-squeezy</p>
             <div class="cta__container">
-              <a class="cta cta--regular" href="index.php?page=">Ga aan de slag</a>
+              <a class="cta cta--regular" href="index.php?page=tutorial">Ga aan de slag</a>
             </div>
           </div>
         </div>
