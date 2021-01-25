789d497098ac615785bcfe118e6f9a72067ccb6b
Kaat Vermeire <kaat.vermeire@student.howest.be>
2021-01-25 16:58:52 +0100

    placing orders with js #52

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,40 @@
+const handleOrderPlacement = e => {
+  const $form = e.currentTarget;
+  e.preventDefault();
+  placeOrder($form.getAttribute('action'), formdataToJson($form));
+};
+
+const placeOrder = async (url, data) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    }),
+    body: JSON.stringify(data)
+  });
+  const returned = await response.json();
+  console.log(returned);
+  if (returned.error) {
+    console.logg(returned.error);
+  }
+};
+
+const formdataToJson = $from => {
+  const data = new FormData($from);
+  const obj = {};
+  data.forEach((value, key) => {
+    console.log(`${key} : ${value}`);
+    obj[key] = value;
+  });
+  return obj;
+};
+
 const init = () => {
+
+  const $ordersForm = document.querySelector(`.form`);
+  if ($ordersForm) {
+    $ordersForm.addEventListener(`submit`, handleOrderPlacement);
+  }
 
 };
 
