d3dac6935f4a34b1c19db0e4ecf58dbb3c0e7047
Kaat Vermeire <kaat.vermeire@student.howest.be>
2021-01-25 12:41:00 +0100

    different values of buttons on screens #11

--- src/view/site/shop.php
+++ src/view/site/shop.php
@@ -22,22 +22,57 @@
     <fieldset class="form__checkboxes">
       <legend class="form__subtitle horizontal__title">Materiaal</legend>
       <div class="form__field">
-        <label class="checkbox__label"><input type="checkbox" name="groot" value="1">Groot wiel</label>
-        <label class="checkbox__label"><input type="checkbox" name="klein" value="2">Klein wiel</label>
-        <label class="checkbox__label"><input type="checkbox" name="stuur" value="3">Stuur</label>
-        <label class="checkbox__label"><input type="checkbox" name="pedals" value="4">Pedalen</label>
-        <label class="checkbox__label"><input type="checkbox" name="zadel" value="5">Zadel</label>
-      </div>
-      <div class="form__field">
-        <label class="checkbox__label"><input type="checkbox" name="staaf" value="1">Ijzeren staaf</label>
-        <label class="checkbox__label"><input type="checkbox" name="ducktape" value="2">Ducktape (3x)</label>
-        <label class="checkbox__label"><input type="checkbox" name="hout" value="3">Houten plank (2x)</label>
-        <div class="form__field">
-        <label class="checkbox__label"><input type="checkbox" name="dop" value="4">Dopmoer (4x)</label>
-        <label class="checkbox__label"><input type="checkbox" name="moer" value="3">Moer (2x)</label>
+        <ul class="form__checkbox-list">
+          <li class="checkbox__item">
+            <input class="opacity" type="checkbox" name="material" value="groot">
+            <label class="checkbox__label">Groot wiel</label>
+          </li>
+          <li class="checkbox__item">
+            <input class="opacity" type="checkbox" name="material" value="klein">
+            <label class="checkbox__label">Klein wiel</label>
+          </li>
+          <li class="checkbox__item">
+            <input class="opacity" type="checkbox" name="material" value="stuur">
+            <label class="checkbox__label">Stuur</label>
+          </li>
+          <li class="checkbox__item">
+            <input class="opacity" type="checkbox" name="material" value="pedaal">
+            <label class="checkbox__label">Pedalen</label>
+          </li>
+          <li class="checkbox__item">
+            <input class="opacity" type="checkbox" name="material" value="zadel">
+            <label class="checkbox__label">Zadel</label>
+          </li>
+          <li class="checkbox__item">
+            <input class="opacity" type="checkbox" name="material" value="ijzer">
+            <label class="checkbox__label">Ijzeren staaf</label>
+          </li>
+          <li class="checkbox__item">
+            <input class="opacity" type="checkbox" name="material" value="ducktape">
+            <label class="checkbox__label">Ducktape (3x)</label>
+          </li>
+          <li class="checkbox__item">
+            <input class="opacity" type="checkbox" name="material" value="hout">
+            <label class="checkbox__label">Houten plank (2x)</label>
+          </li>
+          <li class="checkbox__item">
+            <input class="opacity" type="checkbox" name="material" value="dop">
+            <label class="checkbox__label">Dopmoer (4x)</label>
+          </li>
+          <li class="checkbox__item">
+            <input class="opacity" type="checkbox" name="material" value="moer">
+            <label class="checkbox__label">Moer (2x)</label>
+          </li>
+        </ul>
       </div>
 
-      <div class="form__buttons">
+      <div class="form__buttons btn__mobile">
+        <input id="prev" type="button" class="opacity form__btn button btn__prev" value="hidden">
+        <input id="next" type="button" class="form__btn button btn__next" value="&#10095;">
+      </div>
+      
+      <div class="form__buttons btn__desktop">
+        <input id="prev" type="button" class="opacity form__btn button btn__prev" value="hidden">
         <input id="next" type="button" class="form__btn button btn__next" value="Persoonlijke Gegevens">
       </div>
     </fieldset>
@@ -56,61 +91,69 @@
 
       <div class="form__field">
         <label>Voornaam
-          <input type="text" required placeholder="Bob" size="50">
+          <input type="text" required placeholder="Bob">
         </label>
         <label>Naam
-          <input type="text" required placeholder="De Bouwer" size="50">
+          <input type="text" required placeholder="De Bouwer">
         </label>
       </div>
 
       <div class="form__field">
         <label>E-mail
-          <input type="email" required placeholder="bob.debouwer@email.com" size="50">
+          <input type="email" required placeholder="bob.debouwer@email.com">
         </label>
       </div>
 
       <div class="form__field">
         <label>Straat
-          <input type="text" required placeholder="Makerlaan" size="50">
+          <input type="text" required placeholder="Makerlaan">
         </label>
         <label> Nummer
-          <input type="number" required placeholder="1" size="10">
+          <input type="number" required placeholder="1">
         </label>
       </div>
 
       <div class="form__field">
         <label>Postcode
-          <input type="number" required placeholder="9000" size="4">
+          <input type="number" required placeholder="9000">
         </label>
         <label>Stad / Gemeente
-          <input type="text" required placeholder="Gent" size="50">
+          <input type="text" required placeholder="Gent">
         </label>
       </div>
 
-      <div class="form__buttons">
+      <div class="form__buttons btn__mobile">
+          <input id="prev" type="button" class="form__btn button btn__prev" value="&#10094;">
+          <input id="next" type="button" class="form__btn button btn__next" value="&#10095;">
+      </div>
+      <div class="form__buttons btn__desktop">
           <input id="prev" type="button" class="form__btn button btn__prev" value="Building Box aanpassen">
-          <input id="next" type="button" class="form__btn button btn__next" value="Betaling">
-        </div>
+          <input id="next" type="button" class="form__btn button btn__next" value="Betalingsmethode">
+      </div>
     </fieldset>
 
     <fieldset class="form__betaling">
       <legend class="form__subtitle horizontal__title">Betaling</legend>
         <div class="payment__wrapper">
           <label>
-            <input type="radio" name="payment" value="1" class="hidden">
+            <input type="radio" name="payment" value="1" class="opacity">
             <img src="./assets/social/bankcontact.jpg" alt="bankcontact" width="120" height="85">
           </label>
           <label>
-            <input type="radio" name="payment" value="2" class="hidden">
+            <input type="radio" name="payment" value="2" class="opacity">
             <img src="./assets/social/payconiq.jpg" alt="payconiq" width="120" height="85">
           </label>
           <label>
-            <input type="radio" name="payment" value="3" class="hidden">
+            <input type="radio" name="payment" value="3" class="opacity">
             <img src="./assets/social/visa.jpg" alt="visa" width="120" height="85">
           </label>
       </div>
 
-      <div class="form__buttons">
+  <div class="form__buttons btn__mobile">
+    <input id="prev" type="button" class="form__btn button btn__prev" value="&#10094;">
+    <input class="form__btn button btn__next form__submit" type="submit" value="&check;">
+  </div>
+  <div class="form__buttons btn__desktop">
     <input id="prev" type="button" class="form__btn button btn__prev" value="Betalingmethode aanpassen">
     <input class="form__btn button btn__next form__submit" type="submit" value="Bestelling plaatsen">
   </div>
